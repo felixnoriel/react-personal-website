1949973837da8802c9d06f7fdbe935c682e6dd0f
felixnoriel <jrnoriel_56@yahoo.com>
2018-08-24 15:39:00 +1000

    update config

--- helpers/config.js
+++ helpers/config.js
@@ -1,10 +1,11 @@
 import getConfig from 'next/config'
 const {publicRuntimeConfig} = getConfig();
 
-let endpoint = 'http://35.227.92.179/wordpress/wp-json/wp/v2';
+let endpointWpAPI = 'http://35.227.92.179/wordpress/wp-json/wp/v2';
+let endpoint = 'http://35.227.92.179';
 
 if(publicRuntimeConfig.node_env === "development"){
-   endpoint = 'http://localhost:3000';    
+   endpoint = 'http://localhost:3000';
 }
 
 export default {
